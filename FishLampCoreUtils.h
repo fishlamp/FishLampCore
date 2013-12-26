e93ffefdabff053455538461fbdc2ed6e9ac620c
Mike Fullerton <mike@greentongue.com>
2013-12-26 10:51:46 -0800

    cleaning up core

---
+++ FishLampCoreUtils.h
@@ -0,0 +1,14 @@
+//
+//  FishLampCoreUtils.h
+//  FishLamp
+//
+//  Created by Mike Fullerton on 12/26/13.
+//
+//
+
+#import "FishLampRequired.h"
+
+// dependencies
+#import "NSString+FishLampCore.h"
+#import "FLBitFlags.h"
+#import "FLMath.h"
